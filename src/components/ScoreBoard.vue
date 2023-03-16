92f87668756ea838e8c31d72eb6960043aca032c
AdrianoJH <adrianorsouza.h6@gmail.com>
2023-03-16 16:24:20 -0300

    first commit

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,42 @@
+<template>
+   
+    <section class="score">
+        <h1>Quiz</h1>
+        <div class="pontos">
+            Player <span>{{ this.winCount }}</span> X <span>{{ this.loseCount }}</span> Computer
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['winCount', 'loseCount']
+}
+</script>
+
+<style lang="scss">
+
+    section.score {
+        border-bottom: 1px solid black;
+        padding: 25px;
+        font-size: 18px;
+       
+
+        h1 {
+            margin-top: -30px;
+            margin-bottom: 70px;
+            font-size: 35px;
+        }
+
+        div.pontos {
+            margin-left: 30px;
+        }
+
+        span {
+            padding: 10px;
+            font-weight: bold;
+            border: 1px solid black;
+        }
+
+    }
+</style>
